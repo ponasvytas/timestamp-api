e1c36a1cd14a5de661ed6ba2a6100064cab73ddc
Vytautas Usaitis <ponasvytas@yahoo.com>
2016-10-08 19:37:03 +0000

    finished exercise

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-const path = require("path")
-var express = require("express")
+var express = require("express");
 var app = express();
+var moment = require("moment");
 
-app.get('/home', function (req, res) {
+function dateEval (date){
+    var re = /\W/g;
+    var dateArr = date.split(re);
+    
+    if (dateArr.length === 1){
+        var m = moment(date);
+        if(m.isValid){
+          return moment.unix(dateArr[0])  
+        }
+        return null;
+    }
+
+     return moment(dateArr.join("-"),["DD-MM-YYYY", "DD-MM-YY"
+                                , "MM-DD-YY","MM-DD-YYYY", "YYYY-MM-DD", "YYYY-DD-MM", 
+                                "DD-MMM-YYYY", "MMM-DD-YYYY", "DD-MMMM-YYYY"]); 
+}
+
+app.get('/:query', function (req, res) {
   
-  //var params = req.params()
+  var date = req.params.query;
   
-  res.send("hello Vytas");
+  var unix = null;
+  var natural = null;
+  
+  var newDate = dateEval(date);
+
+ if(newDate.format() !="Invalid date"){
+    unix = newDate.format("X");
+    natural = newDate.format("MMMM DD, YYYY");
+ }
+  
+  
+  var obj = {
+      unix: unix,
+      natural: natural, 
+     
+  }
+  
+  res.send(JSON.stringify(obj));
 });
 
 app.listen(8080, function () {
