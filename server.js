3e1992d0d48fb5f79ea9b9afc291268b8d15c3db
Vytautas Usaitis <ponasvytas@users.noreply.github.com>
2019-07-12 19:53:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,46 @@
-var path = require("path");
-var express = require("express");
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+const moment = require('moment');
 var app = express();
-var moment = require("moment");
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-var port = process.env.PORT || 8080
-
-
-
-app.get('/', function(req, res) {
-  var fileName = path.join(__dirname, '/index.html');
-  res.sendFile(fileName, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', fileName);
-    }
-  });
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// // set the view engine to ejs
-// app.set('view engine', 'ejs');
-
-// // make express look in the public directory for assets (css/js/img)
-// app.use(express.static(__dirname + '/timestamp'));
-
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
 
 function dateEval (date){
-    var re = /\W/g;
-    var dateArr = date.split(re);
-    
-    if (dateArr.length === 1){
-        var m = moment(date);
-        if(m.isValid){
-          return moment.unix(dateArr[0])  
-        }
-        return null;
-    }
+  var re = /\W/g;
+  var dateArr = date.split(re);
 
-     return moment(dateArr.join("-"),["DD-MM-YYYY", "DD-MM-YY"
-                                , "MM-DD-YY","MM-DD-YYYY", "YYYY-MM-DD", "YYYY-DD-MM", 
-                                "DD-MMM-YYYY", "MMM-DD-YYYY", "DD-MMMM-YYYY"]); 
+  if (dateArr.length === 1){
+    var m = moment(date);
+    if(m.isValid){
+      return moment.unix(dateArr[0])  
+    }
+    return null;
+  }
+   return moment(dateArr.join("-"),["DD-MM-YYYY", "DD-MM-YY"
+                              , "MM-DD-YY","MM-DD-YYYY", "YYYY-MM-DD", "YYYY-DD-MM", 
+                              "DD-MMM-YYYY", "MMM-DD-YYYY", "DD-MMMM-YYYY"]); 
 }
 
-app.get('/:query', function (req, res) {
+app.get('/api/timestamp/:query', function (req, res) {
   
   var date = req.params.query;
   
@@ -56,21 +49,20 @@ app.get('/:query', function (req, res) {
   
   var newDate = dateEval(date);
 
- if(newDate.format() !="Invalid date"){
+  if(newDate.format() !="Invalid date"){
     unix = newDate.format("X");
     natural = newDate.format("MMMM DD, YYYY");
- }
-  
-  
+  }
+
   var obj = {
       unix: unix,
-      natural: natural, 
-     
+      natural: natural
   }
   
-  res.send(JSON.stringify(obj));
+  res.json(obj);
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
